4af973a775cef45c3e2863b927bb020c8373dfa1
oolong <bluewoodward@outlook.com>
2022-08-13 16:46:11 +0100

    initial commit

---
+++ scratchpad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace scratchpad
+{
+    class Blob //Class Declaration
+    {
+        public int WeightKg = 1;  //Property
+        public string Name = "tim"; //Property
+
+        public Blob(string name, int weight) //Constructor (same name as the class and no return type) 
+        {
+            Name = name;
+            WeightKg = weight;
+        }
+
+        public void Display() //Method (functions attatched to a class)
+        {
+            Console.WriteLine($"Hello {Name} with weight {WeightKg}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blob blobvar = new Blob("john",2);
+            Blob blobvar2 = new Blob("tim",1);
+            Blob blobvar3 = new Blob("abraham",3);
+            blobvar.Display();
+            blobvar2.Display();
+            blobvar3.Display();
+        }
+    }
+}
